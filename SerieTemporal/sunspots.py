2cb298daee5f13853400bf4c2ab2db5d8676d726
Ian Pacini <ianpacini@hotmail.com>
2023-10-16 18:25:45 -0300

    Code almost finished, needs bugfixing

--- SerieTemporal/sunspots.py
+++ SerieTemporal/sunspots.py
@@ -3,11 +3,11 @@ import matplotlib.pyplot as plt
 
 ds = np.loadtxt("DataSets/serieTemporal\sunspot/sunspots.dat", delimiter="\t")
 
-timeF = ds[:, 0]
-valueF = ds[:, -1]
+timeFoo = ds[:, 0]
+valueFoo = ds[:, -1]
 
-time = timeF[:100]
-value = valueF[:100]
+time = timeFoo[:100]
+value = valueFoo[:100]
 
 plt.plot(time,value)
 plt.show()
