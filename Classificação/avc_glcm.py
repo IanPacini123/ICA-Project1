ab8e3ff4fe1a2eccfc4ea4696468037a34b50af9
Ian Pacini <124642552+IanPacini123@users.noreply.github.com>
2023-10-17 08:40:25 -0300

    Test Fix.

--- Classificação/avc_glcm.py
+++ Classificação/avc_glcm.py
@@ -15,8 +15,8 @@ X_train_raw, X_test_raw, y_train_raw, y_test_raw = train_test_split(X, y, strati
 X_train = np.column_stack((X_train_raw, np.ones(X_train_raw.shape[0])))
 X_test = np.column_stack((X_test_raw, np.ones(X_test_raw.shape[0])))
 
-y_train = y_train_raw.reshape(y_train_raw.shape[0], -1)
-y_test = y_test_raw.reshape(y_test_raw.shape[0], -1)
+y_train = y_train_raw
+y_test = y_test_raw
 
 MLP = MultiLayerPerceptron(X_train=X_train, X_test=X_test, y_train=y_train, y_test=y_test, activate_func='step', epochs=5000)
 
