d56f0f70aeb4cec5a02c1538f51f6cb94a6035e8
Ian Pacini <ianpacini@hotmail.com>
2023-10-23 12:30:58 -0300

    Applied algorithms. No smart analysis. No Plots.

---
+++ Classificação/real/avc_glcm/SVR.py
@@ -0,0 +1,96 @@
+## Data Partition
+from avc_glcm_data_partition import Avc_glcm_data
+
+dataSet = Avc_glcm_data()
+
+X_train = dataSet.X_train_raw
+X_test = dataSet.X_test_raw
+
+y_train = dataSet.y_train_raw
+y_test = dataSet.y_test_raw
+
+## SRV - RBF
+def svr_rbf():
+    from sklearn.svm import SVC
+
+    SVM = SVC(kernel='rbf')
+
+    print("Started Training...")
+    SVM.fit(X_train, y_train)
+
+    print("\nStarted Testing...")
+    # Predict training to check for overfitting
+    y_train_prediction = SVM.predict(X_train)
+
+    error = (len(y_train) - sum(y_train_prediction == y_train)) / len(y_train)
+
+    print("\nTraining Data:")
+    print(f"Training Error: {error}")
+    print(f"Training Acurracy: {1 - error}")
+
+    # Predict test
+    y_test_prediction = SVM.predict(X_test)
+
+    error = (len(y_test) - sum(y_test_prediction == y_test)) / len(y_test)
+
+    print("\nTesting Data:")
+    print(f"Testing Error: {error}")
+    print(f"Testing Acurracy: {1 - error}") 
+
+## SRV - Polynomial
+def svr_poly():
+    from sklearn.svm import SVC
+
+    SVM = SVC(kernel='poly')
+
+    print("Started Training...")
+    SVM.fit(X_train, y_train)
+
+    print("\nStarted Testing...")
+    # Predict training to check for overfitting
+    y_train_prediction = SVM.predict(X_train)
+
+    error = (len(y_train) - sum(y_train_prediction == y_train)) / len(y_train)
+
+    print("\nTraining Data:")
+    print(f"Training Error: {error}")
+    print(f"Training Acurracy: {1 - error}")
+
+    # Predict test
+    y_test_prediction = SVM.predict(X_test)
+
+    error = (len(y_test) - sum(y_test_prediction == y_test)) / len(y_test)
+
+    print("\nTesting Data:")
+    print(f"Testing Error: {error}")
+    print(f"Testing Acurracy: {1 - error}") 
+
+## SVR - Linear
+def svr_linear():
+    from sklearn.svm import SVC
+
+    SVM = SVC(kernel='linear')
+
+    print("Started Training...")
+    SVM.fit(X_train, y_train)
+
+    print("\nStarted Testing...")
+    # Predict training to check for overfitting
+    y_train_prediction = SVM.predict(X_train)
+
+    error = (len(y_train) - sum(y_train_prediction == y_train)) / len(y_train)
+
+    print("\nTraining Data:")
+    print(f"Training Error: {error}")
+    print(f"Training Acurracy: {1 - error}")
+
+    # Predict test
+    y_test_prediction = SVM.predict(X_test)
+
+    error = (len(y_test) - sum(y_test_prediction == y_test)) / len(y_test)
+
+    print("\nTesting Data:")
+    print(f"Testing Error: {error}")
+    print(f"Testing Acurracy: {1 - error}") 
+
+svr_rbf()
---
+++ Classificação/real/ban/MLP.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+## Data Partition
+from ban_data_partition import Ban_data
+
+dataSet = Ban_data()
+
+X_train = dataSet.X_train_raw
+X_test = dataSet.X_test_raw
+
+y_train = dataSet.y_train_raw
+y_test = dataSet.y_test_raw
+
+## MLP
+def mlp():
+    from sklearn.neural_network import MLPClassifier
+
+    X_train_w_bias = np.column_stack((X_train, np.ones(X_train.shape[0])))
+    X_test_w_bias = np.column_stack((X_test, np.ones(X_test.shape[0])))
+
+    MLP = MLPClassifier(hidden_layer_sizes=int(dataSet.X.shape[0]/2), activation='relu', solver='adam', max_iter=500, learning_rate_init=0.1, random_state=42)
+
+    print("Started Training...")
+    MLP.fit(X_train_w_bias, y_train)
+    
+    print("\nStarted Testing...")
+    # Predict training to check for overfitting
+    y_train_prediction = MLP.predict(X_train_w_bias)
+
+    error = (len(y_train) - sum(y_train_prediction == y_train)) / len(y_train)
+
+    print("\nTraining Data:")
+    print(f"Training Error: {error}")
+    print(f"Training Acurracy: {1 - error}")
+
+    # Predict test
+    y_test_prediction = MLP.predict(X_test_w_bias)
+
+    error = (len(y_test) - sum(y_test_prediction == y_test)) / len(y_test)
+
+    print("\nTesting Data:")
+    print(f"Testing Error: {error}")
+    print(f"Testing Acurracy: {1 - error}")
+
+mlp()
